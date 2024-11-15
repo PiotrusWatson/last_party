3661c2b6d6d9b63eff62816549ea88aa9a6fb74b
Piotrus Watson <piotruswatson@gmail.com>
2024-11-15 16:39:41 +0200

    made dialogue lists

---
+++ scripts/dialogues.gd
@@ -0,0 +1,28 @@
+extends Node
+
+var greetings = [
+	"Wow, you seem interesting!",
+	"Hello there!",
+	"Let's talk!",
+	"What's up?",
+	"How are you?"
+]
+
+var initiators = [
+	"So, how do you know the host?",
+	"Tell me your hopes and or dreams",
+	"You seem really quiet, are you doing ok?",
+]
+
+var rants = [
+	"Did you know that the British Rail Class 58 had a brief stint in Spain in 2000?",
+	"I can't believe my coworker told me"
+]
+
+var responses = [
+	"Mmhmm",
+	"Yeah...",
+	"Wow...",
+	"Really!!!",
+	"Insane..."
+]
