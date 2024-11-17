06941d7dc3e76b64dcc2f4823cb858d7bd49f643
Piotrus Watson <piotruswatson@gmail.com>
2024-11-17 12:37:11 +0200

    cleared out some stuff were starting fresh

--- prefabs/partygoer/head_pivot.gd
+++ prefabs/partygoer/head_pivot.gd
@@ -16,11 +16,7 @@ func _process(delta: float) -> void:
 	pass
 
 func rotate_clamped(desired_rotation: float):
-	print("current desired rotation", desired_rotation)
-	var limits = pos_limits
-	if desired_rotation > deg_to_rad(pos_limits.x) and desired_rotation < deg_to_rad(pos_limits.y):
-		#head.scale.x = -1
-		limits = neg_limits
+	desired_rotation = 0
+	rotation = 0
 
 	
-	global_rotation = clamp(desired_rotation, limits.x, limits.y)
