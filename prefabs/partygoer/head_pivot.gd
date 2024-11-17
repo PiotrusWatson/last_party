bc2b89b5254f2053a4bec7519b3651c23dc396f1
Piotrus Watson <piotruswatson@gmail.com>
2024-11-17 12:47:19 +0200

    ok please work

--- prefabs/partygoer/head_pivot.gd
+++ prefabs/partygoer/head_pivot.gd
@@ -2,13 +2,12 @@ extends Marker2D
 
 @export var pos_limits: Vector2
 @onready var head = $Head
-var neg_limits: Vector2
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pos_limits.x = deg_to_rad(pos_limits.x)
 	pos_limits.y = deg_to_rad(pos_limits.y)
-	neg_limits.x = -1 * pos_limits.x
-	neg_limits.y = -1 * pos_limits.y
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,7 +15,11 @@ func _process(delta: float) -> void:
 	pass
 
 func rotate_clamped(desired_rotation: float):
-	desired_rotation = 0
-	rotation = 0
+	if desired_rotation < pos_limits.x or desired_rotation > pos_limits.y:
+		scale.x = -1
+	else:
+		scale.x = 1
+		
+	rotation = clampf(desired_rotation, pos_limits.x, pos_limits.y) * scale.x
 
 	
