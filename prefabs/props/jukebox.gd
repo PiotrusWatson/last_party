583b8c71b7ebe6eb35ba68729e6671190bfef628
Piotrus Watson <piotruswatson@gmail.com>
2024-11-17 20:58:21 +0200

    radio works

--- prefabs/props/jukebox.gd
+++ prefabs/props/jukebox.gd
@@ -2,8 +2,16 @@ extends Node2D
 
 @export var tracks :Array[AudioStreamMP3]
 @onready var audio_player = $AudioStreamPlayer2D
+@onready var interactable = $Interactable
 # Called when the node enters the scene tree for the first time.
 var current_track = 0
+
+func _ready():
+	play_track()
+
+func interact():
+	increase_track()
+	play_track()
 
 func increase_track():
 	current_track += 1
@@ -12,3 +20,11 @@ func increase_track():
 func play_track():
 	audio_player.stream = tracks[current_track]
 	audio_player.play()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		interactable.make_interactable(body, true, self)
+		
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		interactable.make_interactable(body, false, self)
