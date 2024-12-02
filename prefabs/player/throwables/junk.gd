e50c767c6758eae6d2981ffbb5d922c48badba39
Piotrus Watson <piotruswatson@gmail.com>
2024-12-02 16:24:39 +0200

    added the 3 throwables

--- prefabs/player/throwables/junk.gd
+++ prefabs/player/throwables/junk.gd
@@ -1,6 +1,8 @@
 extends RigidBody2D
 
 var is_ready = false
+
+@export var ammo_type: Globals.AmmoType
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -15,7 +17,9 @@ func _on_pickup_radius_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player") and body.has_method("pickup") and is_ready:
 		body.pickup()
 		queue_free()
-
-
+	
+	if ammo_type == Globals.AmmoType.BEER:
+		queue_free()	
+	
 func _on_time_until_ready_timeout() -> void:
 	is_ready = true
